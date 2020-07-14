814aa248e3a7bdd67b2a6c5315287bd84fb2da85
Rodrigo Castilho <rodrigo.m.castilho@outlook.com>
2020-07-14 15:18:48 -0300

    Adding new game and hit functions

--- src/components/Buttons.js
+++ src/components/Buttons.js
@@ -1,35 +1,51 @@
 import React, { Component } from 'react';
 import CardDeck from './CardDeck';
+import { PlayerHand, DealerHand } from './Hands';
+
 
 class NewGame extends Component {
 
     handleNewGame() {
         CardDeck().cardDeckCreator();
-        CardDeck().dealCard();
-        CardDeck().dealCard();
+        PlayerHand().dealPlayerCard();
+        PlayerHand().dealPlayerCard();
+        DealerHand().dealDealerCard();
     }
 
     render() {
         return(
-            <button className="control-button">
+            <button className="control-button" onClick={ this.handleNewGame }>
+                New Game
             </button>
         );
     }
 }
 
 class HitMe extends Component {
+    
+    handleHitMe() {
+        PlayerHand().dealPlayerCard();
+    }
+    
     render() {
         return(
-            <button className="control-button">
+            <button className="control-button" onClick={ this.handleHitMe }>
+                Hit me
             </button>
         );
     }
 }
 
 class Stand extends Component {
+
+    handleStand() {
+
+    }
+
     render() {
         return(
-            <button className="control-button">
+            <button className="control-button" onClick={ this.handleStand }>
+                Stand
             </button>
         );
     }
